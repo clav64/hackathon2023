14ae3062f52f4e62db3ecbe4d689505b95ce1f69
hefonthefjords <55620414+hefonthefjords@users.noreply.github.com>
2023-09-07 22:18:38 +0100

    WIP - no improvements, still testing stuff. moving bakc to laptop.

--- js/stopwatch.js
+++ js/stopwatch.js
@@ -14,12 +14,29 @@ var sw = {
 
             document.getElementById("body") = "Sorry, this browser is not compatible with the applicaiton.";
             console.log("No browser storage available. Cannot run.");
-            break;
-
+            
+            if (sw.read(lastRun) == "undefined") {
+                sw.store(lastRun, Date.now());
+            }
         }
         else {
             // Clear session storage for new session
             sessionStorage.clear();
+
+            // check if it's a new day
+            let today = Date.now();
+
+            let lastRun = sw.read("lastRun");
+
+            if (lastRun >= (Date.now() - 86400000)) {
+
+                // reset stored values if it's a new day
+                //sw.reset();
+
+            }
+
+            sw.store("lastRun", Date.now());
+
         }
 
         
@@ -34,6 +51,9 @@ var sw = {
       sw.ego.onclick = sw.startWork;
       sw.erst.disabled = false;
       sw.ego.disabled = false;
+
+      // START
+      sw.startWork();
     },
   
     // (C) START WORKING TIME (countup)
@@ -56,7 +76,8 @@ var sw = {
     tickForward : () => {
       // (E1) CALCULATE HOURS, MINS, SECONDS
       sw.now++;
-      let hours = 0, mins = 0, secs = 0,
+        //let hours = sw.read(workHours), mins = sw.read(workMins), secs = sw.read(workSecs),
+            let hours = 0, mins = 0, secs = 0,
       remain = sw.now;
       hours = Math.floor(remain / 3600);
       remain -= hours * 3600;
@@ -70,11 +91,20 @@ var sw = {
       if (secs<10) { secs = "0" + secs; }
         sw.etime.innerHTML = hours + ":" + mins + ":" + secs;
 
+        /*// store work hours
+        sw.store(workHours, hours);
+
+        // store work mins
+        sw.store(workMins, mins);
+
+        // store work secs
+        sw.store(workSecs, secs);*/
     },
 
     tickBackward : () => {
         // (E1) CALCULATE HOURS, MINS, SECONDS
         sw.now--;
+        //let hours = sw.read(lifeHours), mins = sw.read(lifeMins), secs = sw.read(lifeSecs),
         let hours = 0, mins = 0, secs = 0,
         remain = sw.now;
         hours = Math.floor(remain / 3600);
@@ -88,29 +118,48 @@ var sw = {
         if (mins<10) { mins = "0" + mins; }
         if (secs<10) { secs = "0" + secs; }
         sw.etime.innerHTML = hours + ":" + mins + ":" + secs;
+
+        /*// store life hours
+        sw.store(lifeHours, hours);
+
+        // store life mins
+        sw.store(lifeMins, mins);
+
+        // store life secs
+        sw.store(lifeSecs, secs);*/
       },
   
     // (F) RESET
     reset : () => {
       if (sw.timer != null) { sw.startLife(); }
-      sw.now = -1;
+        sw.now = -1;
+        /*// reset work stored values
+        sw.store(workHours, 0);
+        sw.store(workMins, 0);
+        sw.store(workSecs, 0);
+
+        // reset life stored values
+        sw.store(lifeHours, 0);
+        sw.store(lifeMins, 0);
+        sw.store(lifeSecs, 0);*/
+
       sw.tickForward();
     },
 
     // (G) STORE DATA
-    store: (key, val) => {
+    store: (storekey, val) => {
     // Write to localStorage - key val pair
-            let key = key.toString();
-            localStorage.setItem(key, val);   
+            let key = storekey.toString();
+            localStorage.setItem(storekey, val);   
     },
 
     // (H) READ DATA
-    read: (key) => {
+    read: (readkey) => {
         // Read from localStorage - select entry by key name, return object
         
-            let data = localStorage.getItem(key);
+            let data = localStorage.getItem(readkey);
             return data;
-    }
+    },
   };
 window.addEventListener("load", sw.init);
 
